2993456fb76a6dea033ebd0d478b77d2b8318068
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-22 19:25:20 +0530

    included search form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
+import SeachForm from './components/SearchForm'
+
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <SeachForm />
+    </>
   );
 }
---
+++ src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SearchForm = () => {
+  return (
+    <main>
+      <div className="search-box">
+        <input type="text" placeholder="Search..." className="search-bar" />
+      </div>
+    </main>
+  );
+};
+
+export default SearchForm;
