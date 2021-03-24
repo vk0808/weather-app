c99b3e9ea3417ef69c32347fbe97bf3b10deed9c
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-03-24 18:42:02 +0530

    update dynamically

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -39,24 +39,33 @@ const Weather = () => {
 
   const { day, date, month, year } = dateBuilder(new Date());
 
+  console.log("weather: ", weather);
+
   if (!weather) {
-    return <h2 className="section-title">oops! can't find book details</h2>;
+    return <h2 className="section-title">oops! can't find weather details</h2>;
   }
 
   return (
-    <div>
-      <div className="location-box">
-        <div className="location">New York City, US</div>
-        <div className="date">
-          <span className="day">{day}, </span>
-          {date} {month} {year}
+    <>
+      {(typeof weather.main !== "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">
+              <span className="day">{day}, </span>
+              {date} {month} {year}
+            </div>
+          </div>
+          <div className="weather-box">
+            <div className="temperature">
+              {weather.main.temp}
+              &#0176;c
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+          </div>
         </div>
-      </div>
-      <div className="weather-box">
-        <div className="temperature">15 &#0176;c</div>
-        <div className="weather">sunny</div>
-      </div>
-    </div>
+      ) : ('')}
+    </>
   );
 };
 
