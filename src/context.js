68e99a45d0396d60c602d285164c5e694772ed6b
Vinod Kumar R <47006421+vk0808@users.noreply.github.com>
2021-04-03 20:22:34 +0530

    get current location

--- src/context.js
+++ src/context.js
@@ -1,31 +1,31 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useCallback } from "react";
 import axios from "axios";
 const url = "https://api.openweathermap.org/data/2.5/weather?";
-// not included api key 
-const api_key = process.env.REACT_APP_API_KEY;
+// not included api key
+const api_key = "57c11981ce67a52344b232e7e273c978";
 
 export const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-  const [query, setQuery] = useState("bangalore");
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState([]);
-  
-  const search = () => {
-    setLoading(true)
+
+  const search = useCallback(() => {
+    setLoading(true);
     axios
       .get(`${url}q=${query}&units=metric&appid=${api_key}`)
       .then((response) => {
         // setQuery("");
         setWeather(response.data);
-        setLoading(false)
+        setLoading(false);
       })
       .catch((error) => {
-        console.log(error);
-        setLoading(false)
+        // console.log(error);
+        setLoading(false);
       });
-  };
-  
+  }, [query]);
+
   useEffect(() => {
     var options = {
       enableHighAccuracy: true,
@@ -34,14 +34,19 @@ export const AppProvider = ({ children }) => {
     };
 
     function success(pos) {
-      const {latitude, longitude} = pos.coords
+      const { latitude, longitude } = pos.coords;
+      setLoading(true);
       axios
-        .get(`${url}lat=${latitude}&lon=${longitude}&units=metric&appid=${api_key}`)
+        .get(
+          `${url}lat=${latitude}&lon=${longitude}&units=metric&appid=${api_key}`
+        )
         .then((response) => {
           setWeather(response.data);
+          setLoading(false);
         })
         .catch((error) => {
-          console.log(error);
+          // console.log(error);
+          setLoading(false);
         });
     }
 
@@ -49,12 +54,9 @@ export const AppProvider = ({ children }) => {
       console.warn(`ERROR(${err.code}): ${err.message}`);
     }
 
-    navigator.geolocation.getCurrentPosition(success, error, options);
-  }, [])
-
-  useEffect(() => {
-    const data = search();
-    return () => data;
+    query
+      ? search()
+      : navigator.geolocation.getCurrentPosition(success, error, options);
   }, [query]);
 
   return (
